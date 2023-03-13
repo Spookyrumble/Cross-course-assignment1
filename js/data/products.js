3430a5b0c2b2d23b0cdf3d3642f2939af6c675dc
HMAsp <hanand55935@stud.noroff.no>
2023-03-13 11:33:51 +0100

    building data array

---
+++ js/data/products.js
@@ -0,0 +1,112 @@
+export const jackets = [
+  {
+    jacketId: 1,
+    name: "Fleece Jacket",
+    size: ["Small", "Medium", "Large"],
+    price: 59.99,
+    details: true,
+    detailText:
+      "This jacket is made from high-quality materials and designed to keep you warm and comfortable in a variety of weather conditions. It features a well designed inner and very durable outer shell, and is available in different sizes. Whether you're hitting the slopes or just running errands around town, this jacket has you covered.",
+    type: "cold",
+    model: "men",
+  },
+  {
+    jacketId: 2,
+    name: "Light Jacket",
+    size: ["Small", "Medium", "Large"],
+    price: 29.99,
+    details: true,
+    detailText:
+      "This jacket is made from high-quality materials and designed to keep you warm and comfortable in a variety of weather conditions. It features a well designed inner and very durable outer shell, and is available in different sizes. Whether you're hitting the slopes or just running errands around town, this jacket has you covered.",
+    type: "wind",
+    model: "men",
+  },
+  {
+    jacketId: 3,
+    name: "Hiking Jacket",
+    size: ["Small", "Medium", "Large"],
+    price: 29.99,
+    details: true,
+    detailText:
+      "This jacket is made from high-quality materials and designed to keep you warm and comfortable in a variety of weather conditions. It features a well designed inner and very durable outer shell, and is available in different sizes. Whether you're hitting the slopes or just running errands around town, this jacket has you covered.",
+    type: "shell",
+    model: "men",
+  },
+  {
+    jacketId: 4,
+    name: "Hunting Jacket",
+    size: ["Small", "Medium", "Large"],
+    price: 59.99,
+    details: true,
+    detailText:
+      "This jacket is made from high-quality materials and designed to keep you warm and comfortable in a variety of weather conditions. It features a well designed inner and very durable outer shell, and is available in different sizes. Whether you're hitting the slopes or just running errands around town, this jacket has you covered.",
+    type: "wind, rain, cold",
+    model: "men",
+  },
+  {
+    jacketId: 5,
+    name: "Wool Jacket",
+    size: ["Small", "Medium", "Large"],
+    price: 59.99,
+    details: true,
+    detailText:
+      "This jacket is made from high-quality materials and designed to keep you warm and comfortable in a variety of weather conditions. It features a well designed inner and very durable outer shell, and is available in different sizes. Whether you're hitting the slopes or just running errands around town, this jacket has you covered.",
+    type: "inner",
+    model: "men",
+  },
+  {
+    jacketId: 6,
+    name: "Sailor Jacket",
+    size: ["Small", "Medium", "Large"],
+    price: 59.99,
+    details: true,
+    detailText:
+      "This jacket is made from high-quality materials and designed to keep you warm and comfortable in a variety of weather conditions. It features a well designed inner and very durable outer shell, and is available in different sizes. Whether you're hitting the slopes or just running errands around town, this jacket has you covered.",
+    type: "wind, rain",
+    model: "women",
+  },
+  {
+    jacketId: 7,
+    name: "Race Jacket",
+    size: ["Small", "Medium", "Large"],
+    price: 49.99,
+    details: true,
+    detailText:
+      "This jacket is made from high-quality materials and designed to keep you warm and comfortable in a variety of weather conditions. It features a well designed inner and very durable outer shell, and is available in different sizes. Whether you're hitting the slopes or just running errands around town, this jacket has you covered.",
+    type: "Rain",
+    model: "women",
+  },
+  {
+    jacketId: 8,
+    name: "Action Jacket",
+    size: ["Small", "Medium", "Large"],
+    price: 49.99,
+    details: true,
+    detailText:
+      "This jacket is made from high-quality materials and designed to keep you warm and comfortable in a variety of weather conditions. It features a well designed inner and very durable outer shell, and is available in different sizes. Whether you're hitting the slopes or just running errands around town, this jacket has you covered.",
+    type: "snow",
+    model: "women",
+  },
+  {
+    jacketId: 9,
+    name: "Wool Jacket",
+    size: ["Small", "Medium", "Large"],
+    price: 39.99,
+    details: true,
+    detailText:
+      "This jacket is made from high-quality materials and designed to keep you warm and comfortable in a variety of weather conditions. It features a well designed inner and very durable outer shell, and is available in different sizes. Whether you're hitting the slopes or just running errands around town, this jacket has you covered.",
+    type: "Inner",
+    model: "women",
+  },
+  {
+    jacketId: 10,
+    name: "Wind Jacket",
+    size: ["Small", "Medium", "Large"],
+    price: 49.99,
+    details: false,
+    detailText:
+      "This jacket is made from high-quality materials and designed to keep you warm and comfortable in a variety of weather conditions. It features a well designed inner and very durable outer shell, and is available in different sizes. Whether you're hitting the slopes or just running errands around town, this jacket has you covered.",
+    type: "wind, rain",
+    model: "women",
+  },
+];
